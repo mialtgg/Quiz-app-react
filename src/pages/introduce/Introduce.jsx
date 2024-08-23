bccc141ff3534dd4769285bc22911567bd259bcb
Mine ALTUĞ <99974782+mialtgg@users.noreply.github.com>
2024-08-23 16:21:46 +0300

    introduce page fixed

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Introduce.css'
+import Dropdown from '../../components/dropdown/Dropdown'
 
 const Introduce = () => {
+  const difficulty =["easy","medium","hard"]
+  const [difficultyChange, setDifficultyChange ] = useState("")
   return (
     <div className='introduce'>
-        Introduce
+        <div className="introduce-container">
+        <div className="introduce-logo">
+            <img src='./mine_logo.png' alt='Mine Logo' />
+            <span className='introduce-text'>Enginar Quiz</span>
+          </div>
+       <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange}/>
+       <div className='introduce-btn'>Hadi Başlayalım</div>
+        </div>
     </div>
   )
 }
