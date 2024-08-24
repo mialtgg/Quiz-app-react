a95b6cf698c0292af00f6f9353a9dae3ad1a5c4f
Mine ALTUĞ <99974782+mialtgg@users.noreply.github.com>
2024-08-24 22:34:42 +0300

    introduce page css fixed

--- src/pages/introduce/Introduce.jsx
+++ src/pages/introduce/Introduce.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import './Introduce.css'
 import Dropdown from '../../components/dropdown/Dropdown'
 import{useNavigate} from 'react-router-dom'
-
+import introSound from '../../assets/sounds/start.mp3';
 const Introduce = () => {
+    useEffect(() => {
+        const audio = new Audio(introSound);
+        audio.play().catch(error => {
+            console.error("Oynatma hatası:", error);
+        });
+
+        // Bileşen kaldırıldığında (sayfa değiştirildiğinde) sesi durdur
+        return () => {
+            audio.pause();
+            audio.currentTime = 0; // Sesin başa dönmesini sağla
+        };
+    }, []);
   const difficulty =["easy","medium","hard"]
   const [difficultyChange, setDifficultyChange ] = useState("")
   const navigate = useNavigate()
@@ -24,7 +36,6 @@ const Introduce = () => {
        <Dropdown data={difficulty} setDifficultyChange={setDifficultyChange}/>
        <div onClick={startQuiz} className='introduce-btn'>Hadi Başlayalım</div>
         </div>
-        <button className="btn btn-primary">Bootstrap Butonu</button>
     </div>
   )
 }
